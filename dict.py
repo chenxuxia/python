3e024247ca47410a1a71dc5b1377ad672754a0bf
icestin <ning231@126.com>
2018-03-26 20:02:25 +0800

    dict

--- dict.py
+++ dict.py
@@ -1,5 +1,8 @@
-s1=set([1,1,2,2,3,3])
-print(s1)
-s2=set([2,3,4])
-print(s1&s2)
-print(s1|s2)
+
+d={
+    'mic':95,
+    'bod':80,
+    'trca':75
+}
+print('d[\'mic\']',d['mic'])
+print('d.get(\'tom\',-1)=',d.get('tom',-1))
